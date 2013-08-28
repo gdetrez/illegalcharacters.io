60e1f1a48ad8a4f49404ff9ac7068d5763fece0e
Grégoire Détrez <gregoire.detrez@gu.se>
2013-08-28 15:46:17 +0200

    Greate prototypee

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,28 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('illegalcharacters.io', []).
+  value('version', '0.1').
+
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/a', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/'});
+  }]).
+
+  factory('backend', function () {
+    return { data: 'abcé´ääêëüë132€15', options: {} };
+  }).
+
+  controller('MyCtrl1', ['$scope', 'backend', function($scope, backend) {
+    $scope.backend = backend;
+  }])
+  .controller('MyCtrl2', ['$scope', 'backend', function($scope, backend) {
+    $scope.txt = '';
+    for ( var i = 0 ; i < backend.data.length ; i++ ) {
+      if ( backend.data.charCodeAt(i) >= 128 )
+        $scope.txt  += '<b>' + backend.data[i] + '</b>';
+      else
+        $scope.txt  += backend.data[i];
+    }
   }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,28 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(module('illegalcharacters.io'));
 
 
-  it('should ....', inject(function() {
-    //spec body
+  it('should mark a non ascii letter', inject(function($rootScope, $controller) {
+    var mockBackend = { data: 'é', options: {}};
+    var scope = $rootScope.$new(),
+        ctrl = $controller("MyCtrl2", { $scope: scope, backend: mockBackend });
+    expect(scope.txt).toBe('<b>é</b>');
   }));
 
-  it('should ....', inject(function() {
-    //spec body
+  it('should not mark an ascii letter', inject(function($rootScope, $controller) {
+    var mockBackend = { data: 'a', options: {}};
+    var scope = $rootScope.$new(),
+        ctrl = $controller("MyCtrl2", { $scope: scope, backend: mockBackend });
+    expect(scope.txt).toBe('a');
   }));
+
+  it('should mark a non ascii letter among ascii ones', inject(function($rootScope, $controller) {
+    var mockBackend = { data: 'aéa', options: {}};
+    var scope = $rootScope.$new(),
+        ctrl = $controller("MyCtrl2", { $scope: scope, backend: mockBackend });
+    expect(scope.txt).toBe('a<b>é</b>a');
+  }));
+
 });
